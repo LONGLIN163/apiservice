554f8d7e4c9bd50dc80d057bc949cf062c56cd81
developerlin123 <developerlin123@gmail.com>
2021-07-19 16:46:56 +0200

    add more

---
+++ Ctrl/adminCtrl.js
@@ -0,0 +1,130 @@
+
+const mysql = require('mysql');
+var formidable = require('formidable');
+
+var db = mysql.createConnection({
+    host: 'blog-db.camhq7invtrq.us-east-2.rds.amazonaws.com',
+    port: '3306',
+    user: 'longlin',
+    password: '12345678',
+    database: 'react_blog'
+  });
+
+exports.checkLogin=function(req,res){
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    //console.log(req)
+    let form = new formidable.IncomingForm()
+    form.parse(req, (err, fields, file) => {
+        console.log(fields)
+        let userName=fields.userName;
+        let password=fields.password;
+  
+        let sql = "SELECT userName FROM admin_user WHERE userName='"+userName + 
+                  "' AND password = '"+password+"'"
+    
+        db.query(sql, function(err,data){
+            let openId=new Date().getTime();
+
+            if(err){
+               console.log("sever error ",err);
+               return;
+            }else if(data.length>0){
+              res.json({
+                    "data":"login success",
+                    "openId":openId
+                })
+            }else{
+              res.json({
+                    "data":"login failed"
+                })
+            }
+
+            console.log("session***************:"+openId)
+        }) 
+    
+    })
+}
+
+
+exports.index=function(req,res){
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    db.query("SELECT * FROM type",function(err,data){
+        if(err){
+          console.log("access type info wrong",err);
+        }else{
+          console.log(data);
+          res.json({"data":data})
+        }
+    })
+}
+
+
+exports.getTypeInfo=function(req,res){
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    db.query("SELECT * FROM type",function(err,data){
+        if(err){
+          console.log("access type info wrong",err);
+        }else{
+          console.log(data);
+          res.json({"data":data})
+        }
+    })
+}
+
+exports.addArticle=function(req,res){
+    let form = new formidable.IncomingForm()
+    form.parse(req, (err, fields, file) => {
+        //const inputValue=fields.input;
+        console.log(fields)
+        const result = mergeLetters(inputValue)
+        //console.log(result)
+        res.json({"result":result})
+    })
+}
+
+
+exports.getArticleList=function(req,res){
+
+    let sql = 'SELECT article.id as id,'+
+    'article.title as title,'+
+    'article.introduce as introduce,'+
+    'article.addTime as addTime,'+
+    'article.view_count as view_count ,'+
+    'type.typeName as typeName '+
+    'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+    'ORDER BY article.id DESC'
+
+    db.query(sql,function(err,data){
+        if(err){
+          console.log("access type info wrong",err);
+        }else{
+          console.log(data);
+          res.json({"data":data})
+        }
+    })  
+}
+
+exports.getArticleById=function(req,res){
+    let id=req.params.id;
+    console.log("id---",id)
+
+    let sql='SELECT article.id as id,'+
+    'article.title as title,'+
+    'article.introduce as introduce,'+
+    'article.article_content as article_content ,'+
+    "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime,"+
+    'article.view_count as view_count ,'+
+    'type.typeName as typeName ,'+
+    'type.id as typeId '+
+    'FROM article LEFT JOIN type ON article.type_id = type.Id '+ 
+    'WHERE article.id='+id
+
+    db.query(sql,function(err,data){
+        if(err){
+          console.log("access type info wrong",err);
+        }else{
+          console.log(data);
+          res.json({"data":data})
+        }
+    })  
+}
--- app.js
+++ app.js
@@ -1,41 +1,25 @@
 const express = require('express')
-const mysql = require('mysql');
 
 const app = express()
 const port = process.env.port || 8081;
-//var mainCtrl=require("./Ctrl/adminCtrl");
+var adminCtrl=require("./Ctrl/adminCtrl");
 
-
-var db = mysql.createConnection({
-  host: 'blog-db.camhq7invtrq.us-east-2.rds.amazonaws.com',
-  port: '3306',
-  user: 'longlin',
-  password: '12345678',
-  database: 'react_blog'
-});
-
-
-app.get('/', (req, res) => {
-  db.query("SELECT * FROM type",function(err,data){
-    if(err){
-      console.log("access type info wrong",err);
-    }else{
-      console.log(data);
-      res.json({"data":data})
-    }
-  })
-  //res.send('Hello Worldhahahahahahhahahhaha!')
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Method', 'post')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.send()
 })
 
 
-//app.get('/',                         adminCtrl.index);
-//app.post('/admin/checkLogin',        adminCtrl.checkLogin);
-//app.get('/admin/getTypeInfo',        adminCtrl.getTypeInfo);
+app.get('/',                         adminCtrl.index);
+app.post('/admin/checkLogin',        adminCtrl.checkLogin);
+app.get('/admin/getTypeInfo',        adminCtrl.getTypeInfo);
 //app.post('/admin/addArticle',        adminCtrl.addArticle);
 //app.post('/admin/updateArticle',     adminCtrl.updateArticle);
-//app.get('/admin/getArticleList',     adminCtrl.getArticleList);
+app.get('/admin/getArticleList',     adminCtrl.getArticleList);
 //app.get('/admin/delArticle/:id',     adminCtrl.delArticle);
-//app.get('/admin/getArticleById/:id', adminCtrl.getArticleById); 
+app.get('/admin/getArticleById/:id', adminCtrl.getArticleById); 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
