d856221efd3a3eaa32ba350ceaaea5607d260d1b
LongLin <developerlin123@gmail.com>
2022-08-02 15:48:22 +0200

    update port for heroku

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express')
 
 const app = express()
-const port = process.env.port || 8081;
+//const port = process.env.port || 8081;
+const port = process.env.PORT || 3001;
 var adminCtrl=require("./Ctrl/adminCtrl");
 var clientCtrl=require("./Ctrl/clientCtrl");
 
-//const pdUrl="https://ll-blog-admin.s3-website.eu-west-3.amazonaws.com";
-const pdUrl="http://localhost:3000";
-
 app.options('*', (req, res) => {
-  //res.header('Access-Control-Allow-Origin', "http://ll-blog-admin.s3-website.eu-west-3.amazonaws.com")
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Method', 'post')
   res.header('Access-Control-Allow-Headers', '*')
